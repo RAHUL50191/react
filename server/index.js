97f0848ac17b8fdd10eaf652abff8dfcafa9211a
Rahul <rahul200283@gmail.com>
2023-05-19 19:54:35 +0530

    /*
    routing for react app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3001;
-
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+// server/index.js
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 app.get("/api", function (req, res) {
   res.json({ msg: "Hello from server!" });
 });
